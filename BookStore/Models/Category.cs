1d11ac189d508c7c0c58606a9dd651992c16ff85
eraybaydur <eray.bydr@hotmail.com>
2023-07-12 14:07:29 +0300

    Edit, Delete Actions,Toastr notifications.

--- BookStore/Models/Category.cs
+++ BookStore/Models/Category.cs
@@ -1,5 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Models
 {
@@ -8,7 +8,11 @@ namespace BookStore.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage = "Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
 
     }
